8683344104a4e12510803ec81e32a6c5dda051b6
philippschenk <brv.vierer@gmail.com>
2023-11-11 12:50:15 +0100

    new

--- gpr3/gpr3.py
+++ gpr3/gpr3.py
@@ -1,24 +1,29 @@
 def main():
     __author__ = "7093700, Schenk"
     try:
+        # INPUT OF NUMBER
         zahl = int(input('Enter a beautiful number: '))
+        # CASE: INPUT = 0
         if zahl == 0:
-            print('The binary number would be: \n0')
+            print('The binary number (8-bit) would be: \n0')
+        # CASE: INPUT UNDER 0
         elif zahl < 0:
-            print('BLABLA')
-        else:
-            binary_list = []
-            while zahl != 0:
-                rest = zahl % 2
-                ohnerest = zahl - rest
-                zahl = ohnerest / 2
-                print(zahl, 'rest is:', rest)
-                binary_list.append(rest)
-            reversed_list = list(reversed(binary_list))
-            # print(reversed_list)
-            print('The binary number would be:')
-            for r in reversed_list:
-                print(str(int(r)), end='')
+            zahl = zahl & (2**8 - 1)
+        # USUAL PROCESSING: DIVIDING BY 2 TIL NO REST
+        binary_list = []
+        while zahl != 0:
+            rest = zahl % 2
+            ohnerest = zahl - rest
+            zahl = ohnerest / 2
+            #print(zahl, 'rest is:', rest)
+            binary_list.append(rest)
+        # REVERSE THE APPENDED LIST AND THEN PRINT EVERY ELEMENT
+        reversed_list = list(reversed(binary_list))
+        # print(reversed_list)
+        print('The binary number (8-bit) would be:')
+        for r in reversed_list:
+            print(str(int(r)), end='')
+    # CASE: INPUT NOT INTEGER
     except:
         print('Input has to be a integer.')
 
