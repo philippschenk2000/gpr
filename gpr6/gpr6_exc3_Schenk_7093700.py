70eb5818c2344ce8b79a9b0decfe1c3da4d90b3d
philippschenk <brv.vierer@gmail.com>
2023-11-21 15:42:53 +0100

    new

--- gpr6/gpr6_exc3_Schenk_7093700.py
+++ gpr6/gpr6_exc3_Schenk_7093700.py
@@ -1,6 +1,17 @@
 def main():
     __author__ = "7093700, Schenk"
 
+    needle = input('needle: ')
+    haystack = input('haystack: ')
+    # INPUT IS EVERY TIME A STRING
+
+    # IF NEEDLE IN HAYSTACK:
+    for i in range(len(haystack)):
+        print(haystack[i:i + len(needle)])
+        if haystack[i:i + len(needle)] == needle:
+            print(-1)
+            break
+
 
 main()
 
