b1de3e656752e5d3b107d07b4ac20f70419589f1
philippschenk <brv.vierer@gmail.com>
2023-11-21 15:46:32 +0100

    new

--- gpr6/gpr6_exc3_Schenk_7093700.py
+++ gpr6/gpr6_exc3_Schenk_7093700.py
@@ -5,13 +5,29 @@ def main():
     haystack = input('haystack: ')
     # INPUT IS EVERY TIME A STRING
 
-    # IF NEEDLE IN HAYSTACK:
+    # ITERATION THRU EVERY CHARACTER IN HAYSTACK:
+    printing = -1
     for i in range(len(haystack)):
-        print(haystack[i:i + len(needle)])
+        #print(haystack[i:i + len(needle)])
         if haystack[i:i + len(needle)] == needle:
-            print(-1)
+            printing = i
+            # WAS ALREADY FOUND
             break
+    print(printing)
 
 
 main()
 
+
+# TEST 1:
+# IN: du, hallo wie gehts dir du knecht
+# SHOULD: -1
+# OUT: -1
+# TEST 2:
+# IN: du, hallo wie gehts dir du knecht
+# SHOULD: -1
+# OUT: -1
+# TEST 3:
+# IN: du, hallo wie gehts dir du knecht
+# SHOULD: -1
+# OUT: -1
