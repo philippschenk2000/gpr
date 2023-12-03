b9d7be8b1d052ed601fbbf42a57946c10a9de7ac
philippschenk <brv.vierer@gmail.com>
2023-12-03 17:43:10 +0100

    new

--- gpr7/gpr7_exc3_Schenk_7093700.py
+++ gpr7/gpr7_exc3_Schenk_7093700.py
@@ -8,14 +8,16 @@ def main():
 
     """
     try:
-        # INPUT OF THE TUPEL
+        # INPUT OF THE TUPLE
         #T = (('Anna', 'Mathematik'), ('Peter', 'Geologie'), ('Sina', 'Mathematik'), ('Sina', 'Geologie'))
         T = eval(input('Enter your tuple: '))
+        # FORMAT THE TUPLE INTO TWO LISTS
         names, study_fields = zip(*T)
         a = len(list(set(names)))
         b = len(list(study_fields))
         most_frequent = ''
         max_frequency = 0
+        # COUNT THE FREQUENCY OF THE STUDY FIELDS AND FIND MOST FREQUENT
         for i in list(set(study_fields)):
             print(i, list(study_fields).count(i))
             if int(list(study_fields).count(i)) > max_frequency:
@@ -23,6 +25,7 @@ def main():
                 max_frequency = int(list(study_fields).count(i))
             elif int(list(study_fields).count(i)) == max_frequency:
                 most_frequent = most_frequent + ',' + i
+        # CREATE TUPLE FOR OUTPUT BASED ON THE EXERCISES
         T_out = (('exercise_a', a), ('exercise_b', b), ('exercise_c', most_frequent))
         print(T_out)
 
